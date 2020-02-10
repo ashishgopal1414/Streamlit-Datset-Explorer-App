bda4f39988b8ceb1d99a17456ed821ec90d11bf9
Imran S M <41140326+immu0001@users.noreply.github.com>
2020-02-10 11:59:38 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,172 @@
+import os
+import streamlit as st
+
+#EDA Packages
+import pandas as pd
+
+#Visualization Packages
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("Agg")
+import seaborn as sns
+
+
+def main():
+	"""Common ML Dataset Explorer"""
+	st.title("Common Machine Learning Dataset Explorer")
+	st.subheader("Simple Data Science Explorer with streamlit")
+
+	html_temp = """
+	<div style="background-color:tomato;"><p style="color:white; font-weight:bold;">By IMRAN S M</p></div>
+	"""
+	st.markdown(html_temp,unsafe_allow_html=True)
+
+	def file_selector(folder_path ="./datasets"):
+		filenames = os.listdir(folder_path)
+		selected_filename = st.selectbox("Select a File",filenames)
+		return os.path.join(folder_path,str(selected_filename))
+
+	filename = file_selector()
+	st.info("You Selected {}".format(filename))
+
+
+	# Read Data
+	df = pd.read_csv(filename)
+
+
+	# Show Dataset
+	if st.checkbox("Show Dataset"):
+		number = st.number_input("Number of Rows to View",1)
+		st.dataframe(df.head(number))
+
+	# Show Columns
+	if st.button("Column Names"):
+		st.write(df.columns)
+
+	# Show Shape Of Dataframe
+	if st.checkbox("Shape Of Dataframe"):
+		data_dim = st.radio("Show Dimension By ",("Rows","Columns"))
+		if data_dim == 'Rows':
+			st.text("Number of Rows")
+			st.write(df.shape[0])
+		elif data_dim == 'Columns':
+			st.text("Number of Columns")
+			st.write(df.shape[1])
+	else:
+		st.write(df.shape)
+
+	# Select Columns
+	if st.checkbox("Select Columns To Show"):
+		all_columns = df.columns.tolist()
+		Selected_columns = st.multiselect("Select",all_columns)
+		new_df = df[Selected_columns]
+		st.dataframe(new_df)
+
+	# Show Values
+	if st.button("Value Counts"):
+		st.text("Value Counts By Target/Class")
+		st.write(df.iloc[:-1].value_counts())
+
+
+	# Show Data Types
+	if st.button("Data Types"):
+		st.write(df.dtypes)	
+
+
+	# Show Summary
+	if st.checkbox("Describe"):
+		st.write(df.describe().T)
+
+
+
+	# Plot And Visualization
+	st.subheader("Data Visualization")
+
+	# Correlation
+	# Seaborn Plot
+
+	if st.checkbox("Correlation Plot[Seaborn]"):
+		st.write(sns.heatmap(df.corr(),annot=True))
+		st.pyplot()
+
+
+
+	
+	
+	# Pie Chart
+	if st.checkbox("Pie Plot"):
+		all_columns_names = df.columns.tolist()
+		if st.button("Generate Plot"):
+			st.success("Generated Pie Plot:")
+			st.write(df.iloc[:,-1].value_counts().plot.pie(autopct="%1.1f%%"))
+			st.pyplot()
+
+
+	# Count Plot 
+
+	if st.checkbox("Plot of Value Counts"):
+		st.text("Value Counts By Target")
+		all_columns_names = df.columns.tolist()
+		primary_col = st.selectbox("Primary Columm to GroupBy",all_columns_names)
+		selected_columns_names = st.multiselect("Select Columns",all_columns_names)
+		if st.button("Plot"):
+			st.text("Generate Plot")
+			if selected_columns_names:
+				vc_plot = df.groupby(primary_col)[selected_columns_names].count()
+			else:
+				vc_plot = df.iloc[:,-1].value_counts()
+			st.write(vc_plot.plot(kind="bar"))
+			st.pyplot()
+			
+
+
+
+
+
+
+
+	# Customizable Plot
+
+	st.subheader("Customizable Plot")
+	all_columns_names = df.columns.tolist()
+	type_of_plot = st.selectbox("Select Type of Plot ",["area", "bar", "line", "hist", "box", "kde"])
+	selected_column_names = st.multiselect("Select Column To Plot",all_columns_names)
+
+	if st.button("Generate  Plot"):
+		st.success("Generated Customizable {} Plot for {}".format(type_of_plot,selected_column_names))
+
+		# Plot By Streamlit
+		if type_of_plot == "area":
+			cust_data = df[selected_column_names]
+			st.area_chart(cust_data)
+
+		elif type_of_plot == "bar":
+			cust_data = df[selected_column_names]
+			st.bar_chart(cust_data)
+
+
+		elif type_of_plot == "line":
+			cust_data = df[selected_column_names]
+			st.line_chart(cust_data)
+
+
+        # Custom Plot
+		elif type_of_plot:
+			cust_plot = df[selected_column_names].plot(kind=type_of_plot)
+			st.write(cust_plot)
+			st.pyplot()
+
+
+
+
+	if st.button("Celebrate!!"):
+		st.balloons()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
